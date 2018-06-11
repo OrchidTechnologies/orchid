9d254d1589b397563b886f1e4944a629db31ec98
Greg Hazel <ghazel@gmail.com>
2018-06-11 16:02:44 -0700

    initial macos vpn app

---
+++ apple/Orchid/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Orchid
+//
+//  Created by Greg Hazel on 6/11/18.
+//  Copyright © 2018 Example. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ apple/OrchidTunnel/PacketTunnelProvider.m
@@ -0,0 +1,86 @@
+//
+//  PacketTunnelProvider.m
+//  OrchidTunnel
+//
+//  Created by Greg Hazel on 6/11/18.
+//  Copyright © 2018 Example. All rights reserved.
+//
+
+#import "PacketTunnelProvider.h"
+
+PacketTunnelProvider *packetTunnelProvider;
+
+@implementation PacketTunnelProvider
+
++ (PacketTunnelProvider*)sharedProvider
+{
+    return packetTunnelProvider;
+}
+
+- (NSDictionary<NSString *,id>*)config
+{
+    return ((NETunnelProviderProtocol*)self.protocolConfiguration).providerConfiguration;
+}
+
+- (void)startTunnelWithOptions:(nullable NSDictionary<NSString *,NSObject *> *)options completionHandler:(void (^)(NSError * __nullable error))completionHandler
+{
+    NSLog(@"startTunnelWithOptions %@", options);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        packetTunnelProvider = self;
+        [super startTunnelWithOptions:options completionHandler:completionHandler];
+
+        [self readPacketsFromTunnel];
+        NEPacketTunnelNetworkSettings *settings = [NEPacketTunnelNetworkSettings.alloc initWithTunnelRemoteAddress:@"127.0.0.1"];
+        settings.IPv4Settings = [NEIPv4Settings.alloc initWithAddresses:@[@"10.7.0.3"] subnetMasks:@[@"255.255.255.0"]];
+        settings.IPv4Settings.includedRoutes = @[NEIPv4Route.defaultRoute];
+
+        // TODO: IPv6
+        //settings.IPv6Settings = [NEIPv6Settings.alloc initWith...
+
+        settings.DNSSettings = [NEDNSSettings.alloc initWithServers:@[@"8.8.8.8", @"8.8.4.4"]];
+
+        NSLog(@"setTunnelNetworkSettings %@", settings);
+        [self setTunnelNetworkSettings:settings completionHandler:^(NSError * _Nullable error) {
+            NSLog(@"setTunnelNetworkSettings error: %@", error);
+            completionHandler(error);
+            [packetTunnelProvider readPacketsFromTunnel];
+        }];
+    });
+}
+
+- (void)stopTunnelWithReason:(NEProviderStopReason)reason completionHandler:(void (^)(void))completionHandler
+{
+    NSLog(@"stopTunnelWithReason %ld", (long)reason);
+    [super stopTunnelWithReason:reason completionHandler:completionHandler];
+    completionHandler();
+    // kill the extension. is this a good idea?
+    exit(0);
+}
+
+- (void)handleAppMessage:(NSData *)messageData completionHandler:(void (^)(NSData *))completionHandler {
+	// Add code here to handle the message.
+}
+
+- (void)sleepWithCompletionHandler:(void (^)(void))completionHandler {
+	// Add code here to get ready to sleep.
+	completionHandler();
+}
+
+- (void)wake {
+	// Add code here to wake up.
+}
+
+- (void)readPacketsFromTunnel
+{
+    [self.packetFlow readPacketsWithCompletionHandler:^(NSArray<NSData *> * _Nonnull packets, NSArray<NSNumber *> * _Nonnull protocols) {
+        for (int i = 0; i < packets.count; i++) {
+            NSData *packet = packets[i];
+            NSNumber *protocol = protocols[i];
+
+            //[self.packetFlow writePackets:@[packet] withProtocols:@[protocol]];
+        }
+        [self readPacketsFromTunnel];
+    }];
+}
+
+@end
