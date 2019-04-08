102ef905604e1f252190c29f2655d69e8a9a1ea0
Patrick Niemeyer <pat@pat.net>
2019-04-08 17:23:34 -0500

    Adapt size of log settings screen for larger screens.

--- app-shared/lib/pages/settings/settings_log_page.dart
+++ app-shared/lib/pages/settings/settings_log_page.dart
@@ -52,90 +52,95 @@ class _SettingsLogPage extends State<SettingsLogPage> {
     var privacyText =
         "Your log file is stored locally and contains your IP address but does not contain personal or identifying information.";
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
+    return Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 600),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
 
-        // The logging control switch
-        Container(
-          color: AppColors.white,
-          height: 56,
-          child: PageTile(
-            title: "Logging enabled",
-            imageName: "assets/images/assignment.png",
-            onTap: () {},
-            trailing: Switch(
-              activeColor: AppColors.purple_3,
-              value: true,
-              //onChanged: (bool value) {},
-              onChanged: null,
-            ),
-          ),
-        ),
-
-        Container(height: 1, color: AppColors.grey_1.withAlpha((0.12*255).round()),),
-
-        // Privacy description
-        Padding(
-            padding: EdgeInsets.only(left: 20, right: 20, top: 16, bottom: 12),
-            child: AppText.body(
-              color: Color(0xff524862),
-              textAlign: TextAlign.left,
-              text: privacyText,
-            )),
-
-        // The log text view
-        Expanded(
-          child: Padding(
-            padding:
-                const EdgeInsets.only(left: 20, right: 20, top: 12, bottom: 32),
-            child: Container(
-              padding: EdgeInsets.all(16.0),
-              child: SingleChildScrollView(
-                reverse: true,
-                child: Text(
-                  _logText,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontFamily: "VT323", fontSize: 16.0),
+            // The logging control switch
+            Container(
+              color: AppColors.white,
+              height: 56,
+              child: PageTile(
+                title: "Logging enabled",
+                imageName: "assets/images/assignment.png",
+                onTap: () {},
+                trailing: Switch(
+                  activeColor: AppColors.purple_3,
+                  value: true,
+                  //onChanged: (bool value) {},
+                  onChanged: null,
                 ),
               ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                border: Border.all(width: 2.0, color: AppColors.neutral_5),
+            ),
+
+            Container(height: 1, color: AppColors.grey_1.withAlpha((0.12*255).round()),),
+
+            // Privacy description
+            Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 16, bottom: 12),
+                child: AppText.body(
+                  color: Color(0xff524862),
+                  textAlign: TextAlign.left,
+                  text: privacyText,
+                )),
+
+            // The log text view
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 20, right: 20, top: 12, bottom: 32),
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    reverse: true,
+                    child: Text(
+                      _logText,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontFamily: "VT323", fontSize: 16.0),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                    border: Border.all(width: 2.0, color: AppColors.neutral_5),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
 
-        // The buttons row
-        Padding(
-          padding: const EdgeInsets.only(bottom: 28),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.all(0),
-                  child: button(
-                      title: "Copy",
-                      imageName: "assets/images/business.png",
-                      onPressed: _onCopyButton)),
-              Padding(
-                  padding: const EdgeInsets.only(left: 0, right: 0),
-                  child: button(
-                      title: "Delete",
-                      imageName: "assets/images/business.png",
-                      onPressed: _confirmDelete)),
-              Padding(
-                  padding: const EdgeInsets.all(0),
-                  child: button(
-                      title: "Save",
-                      imageName: "assets/images/business.png",
-                      onPressed: null /*_onSave*/)),
-            ],
-          ),
-        )
-      ],
+            // The buttons row
+            Padding(
+              padding: const EdgeInsets.only(bottom: 28),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: button(
+                          title: "Copy",
+                          imageName: "assets/images/business.png",
+                          onPressed: _onCopyButton)),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 0, right: 0),
+                      child: button(
+                          title: "Delete",
+                          imageName: "assets/images/business.png",
+                          onPressed: _confirmDelete)),
+                  Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: button(
+                          title: "Save",
+                          imageName: "assets/images/business.png",
+                          onPressed: null /*_onSave*/)),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
