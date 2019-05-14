f9092ad24c1d2723dbca878519f8e08315ac737d
Jay Freeman (saurik) <saurik@saurik.com>
2019-05-14 02:10:27 -0700

    First implementation of Orchid directory contract.

--- dir-ethereum/directory.sol
+++ dir-ethereum/directory.sol
@@ -24,68 +24,245 @@ pragma solidity ^0.5.7;
 
 import "../openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract OrchidDirectory {
-    ERC20 Orchid_ = ERC20(0x0);
+interface IOrchidDirectory {
+    function have() external view returns (uint64 amount);
+}
 
-    //event Support(address owner, uint256 capability);
+contract OrchidDirectory is IOrchidDirectory {
+
+    ERC20 private orchid_;
+
+    constructor(address orchid) public {
+        orchid_ = ERC20(orchid);
+    }
+
+
+
+    struct Pointer {
+        address staker_;
+        address stakee_;
+    }
+
+    function copy(Pointer storage pointer, address staker, address stakee) private {
+        pointer.staker_ = staker;
+        pointer.stakee_ = stakee;
+    }
+
+    function copy(Pointer storage pointer, Pointer storage other) private {
+        copy(pointer, other.staker_, other.stakee_);
+    }
+
+    function kill(Pointer storage pointer) private {
+        copy(pointer, address(0), address(0));
+    }
+
+    function nope(Pointer storage pointer) private view returns (bool) {
+        return pointer.staker_ == address(0);
+    }
+
+    function name(address staker, address stakee) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(staker, stakee));
+    }
+
+    function name(Pointer storage pointer) private view returns (bytes32) {
+        return name(pointer.staker_, pointer.stakee_);
+    }
+
+
 
     struct Medallion {
-        uint256 network_;
-        //mapping(uint256 => bytes) capabilities_;
+        uint64 before_;
+        uint64 after_;
+
+        uint64 amount_;
+
+        bytes32 parent_;
+        Pointer left_;
+        Pointer right_;
     }
 
-    mapping(address => Medallion) public medallions_;
+    mapping(bytes32 => Medallion) private medallions_;
 
-    struct Weight {
-        address owner_;
-        uint staked_;
+    Pointer private root_;
+
+
+    function have() public view returns (uint64 amount) {
+        if (nope(root_))
+            return 0;
+        Medallion storage medallion = medallions_[name(root_)];
+        return medallion.before_ + medallion.after_ + medallion.amount_;
     }
 
-    Weight[] weights_;
+    function scan(uint128 percent) public view returns (address) {
+        uint64 point = uint64(have() * uint256(percent) / 2**128);
 
-    function stake(address owner, uint staked, uint256 network) public returns (uint index) {
-        require(staked >= 0);
+        Pointer storage pointer = root_;
+        for (;;) {
+            require(!nope(pointer));
+            Medallion storage medallion = medallions_[name(pointer)];
 
-        Medallion memory medallion;
-        medallion.network_ = network;
-        medallions_[owner] = medallion;
+            if (point < medallion.before_) {
+                pointer = medallion.left_;
+                continue;
+            }
 
-        Weight memory weight;
-        weight.owner_ = owner;
-        weight.staked_ = staked;
+            point -= medallion.before_;
 
-        index = weights_.push(weight) - 1;
+            if (point < medallion.amount_)
+                return pointer.stakee_;
 
-        require(Orchid_.transferFrom(msg.sender, address(this), staked));
-    }
+            point -= medallion.amount_;
 
-    function take(uint index, address payable target, uint8 v, bytes32 r, bytes32 s) public {
-        require(index < weights_.length);
-
-        bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(target))));
-        address owner = ecrecover(hash, v, r, s);
-        require(owner == weights_[index].owner_);
-
-        uint256 staked = weights_[index].staked_;
-        require(staked != 0);
-        weights_[index].staked_ = 0;
-
-        require(Orchid_.transfer(target, staked));
-    }
-
-    function random(uint percent) public view returns (uint256) {
-        uint e = weights_.length;
-
-        uint stop = 0;
-        for (uint i = 0; i != e; ++i)
-            stop += weights_[i].staked_;
-        stop = stop * percent / 10**18;
-
-        for (uint i = 0; i != e; ++i) {
-            stop -= weights_[i].staked_;
-            if (stop <= 0)
-                return medallions_[weights_[i].owner_].network_;
+            pointer = medallion.right_;
         }
-        return 0;
     }
+
+
+    function step(bytes32 key, Medallion storage medallion, uint64 amount, bytes32 root) private {
+        while (medallion.parent_ != root) {
+            bytes32 parent = medallion.parent_;
+            medallion = medallions_[parent];
+            if (name(medallion.left_) == key)
+                medallion.before_ += amount;
+            else
+                medallion.after_ += amount;
+            key = parent;
+        }
+    }
+
+    function done(bytes32 key, Medallion storage medallion, uint64 amount) private {
+        require(amount != 0);
+        medallion.amount_ += amount;
+        step(key, medallion, amount, bytes32(0));
+        require(orchid_.transferFrom(msg.sender, address(this), amount));
+    }
+
+    function make(address stakee, uint64 amount) public {
+        address staker = msg.sender;
+        bytes32 key = name(staker, stakee);
+        Medallion storage medallion = medallions_[key];
+        require(medallion.amount_ == 0);
+
+        bytes32 parent = bytes32(0);
+        Pointer storage pointer = root_;
+
+        while (!nope(pointer)) {
+            parent = name(pointer);
+            Medallion storage current = medallions_[parent];
+            pointer = current.before_ < current.after_ ? current.left_ : current.right_;
+        }
+
+        medallion.parent_ = parent;
+        copy(pointer, staker, stakee);
+
+        done(key, medallion, amount);
+    }
+
+    function push(address stakee, uint64 amount) public {
+        address staker = msg.sender;
+        bytes32 key = name(staker, stakee);
+        Medallion storage medallion = medallions_[key];
+        require(medallion.amount_ != 0);
+
+        done(key, medallion, amount);
+    }
+
+
+
+    struct Pending {
+        uint256 time_;
+        uint64 amount_;
+    }
+
+    mapping(address => mapping(uint => Pending)) private pendings_;
+
+    function pull(address stakee, uint64 amount, uint256 index) public {
+        address staker = msg.sender;
+        bytes32 key = name(staker, stakee);
+        Medallion storage medallion = medallions_[key];
+
+        require(amount != 0);
+        require(medallion.amount_ >= amount);
+        medallion.amount_ -= amount;
+
+        step(key, medallion, -amount, bytes32(0));
+
+        if (medallion.amount_ == 0) {
+            if (medallion.parent_ == bytes32(0))
+                delete root_;
+            else {
+                Medallion storage current = medallions_[medallion.parent_];
+                Pointer storage pivot = name(current.left_) == key ? current.left_ : current.right_;
+                Pointer storage child = medallion.before_ > medallion.after_ ? medallion.left_ : medallion.right_;
+
+                if (nope(child))
+                    kill(pivot);
+                else {
+                    Pointer storage last = child;
+                    for (;;) {
+                        current = medallions_[name(last)];
+                        Pointer storage next = current.before_ > current.after_ ? current.left_ : current.right_;
+                        if (nope(next)) {
+                            if (current.parent_ != key)
+                                medallions_[name(child)].parent_ = name(last);
+                            (medallion.parent_, current.parent_) = (current.parent_, medallion.parent_);
+
+                            current.before_ = medallion.before_;
+                            current.after_ = medallion.after_;
+                            current.left_ = medallion.left_;
+                            current.right_ = medallion.right_;
+
+                            copy(pivot, last);
+                            copy(last, staker, stakee);
+                            step(key, medallion, -current.amount_, current.parent_);
+                            kill(last);
+                            break;
+                        }
+
+                        last = next;
+                    }
+                }
+            }
+
+            delete medallions_[key];
+        }
+
+        Pending storage pending = pendings_[msg.sender][index];
+        require(pending.amount_ == 0);
+        pending.time_ = block.timestamp + 30 days;
+        pending.amount_ = amount;
+    }
+
+    function take(uint256 index, address payable target) public {
+        Pending memory pending = pendings_[msg.sender][index];
+        require(pending.amount_ != 0);
+        require(pending.time_ <= block.timestamp);
+        delete pendings_[msg.sender][index];
+        require(orchid_.transfer(target, pending.amount_));
+    }
+
+
+
+    struct Location {
+        uint256 time_;
+        bytes data_;
+    }
+
+    mapping(address => Location) private locations_;
+
+    function move(bytes memory data) public {
+        Location storage location = locations_[msg.sender];
+        location.time_ = block.timestamp;
+        location.data_ = data;
+    }
+
+    function stop() public {
+        delete locations_[msg.sender];
+    }
+
+    function look(address stakee) public view returns (uint256 time, bytes memory data) {
+        Location storage location = locations_[stakee];
+        return (location.time_, location.data_);
+    }
+
 }
