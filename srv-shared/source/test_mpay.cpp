d5d71008fac2a0039310db86beac44d6199a9fc7
jake cannell <jake.cannell@gmail.com>
2019-05-16 15:44:33 -0700

    test_mpay: testing using eth_sendTransaction to deploy binary contracts compiled by solc (works for simple binary code examples, currently getting invalid opcode for tok-ethereum/OrchidToken.bin

--- srv-shared/source/test_mpay.cpp
+++ srv-shared/source/test_mpay.cpp
@@ -22,7 +22,9 @@
 
 #include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <future>
+#include <string>
 
 #include "crypto.hpp"
 #include "jsonrpc.hpp"
@@ -32,9 +34,100 @@
 
 namespace orc {
 
+	using std::string;
+
+
+	std::string file_to_string(std::string fn)
+	{
+		std::ifstream myfile;
+		myfile.open(fn.c_str());
+		if (myfile.is_open()) {
+			std::stringstream buffer;
+			buffer << myfile.rdbuf();
+			return buffer.str();
+		} else {
+			printf("failed to open infile %s \n", fn.c_str());
+			return "";
+		}
+		myfile.close();
+		return "";
+	}
+
+	bool string_to_file(std::string x, std::string fn) {
+		bool bresult;
+		std::ofstream myfile;
+		myfile.open(fn);
+		if (myfile.is_open()) {
+			myfile << x;
+			bresult = true;
+		} else {
+			printf("failed to open outfile %s \n", fn.c_str());
+			bresult = false;
+		}
+		myfile.close();
+		return bresult;
+	}
+
     void test()
     {
         //return Wait([&]() -> task<int> { co_await Schedule(); Endpoint endpoint({"http", "localhost", "8545", "/"}); /* code here */ }() );
+    }
+
+
+    task<int> test_lottery()
+    {
+        Endpoint endpoint({"http", "localhost", "8545", "/"});
+
+        co_await endpoint("web3_clientVersion", {});
+
+
+	    // this assumes testrpc started as:
+	    // testrpc -d --network-id 10
+	    // to get deterministic test accounts (`testAcc` is the first account)
+	    // --network-id 10 is needed to workaround
+	    // https://github.com/ethereum/web3.js/issues/932 (wtf)
+
+        printf("[%d] Example start\n", __LINE__);
+
+
+        // this is the account which sets up the orchid contracts
+        // taken from testrpc
+        const string orchid_address = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1";
+        const string orchid_privkey = "4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d";
+
+        const string server_address = "0xffcf8fdee72ac11b5c542428b35eef5769c409f0";
+        const string server_privkey = "6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1";
+
+   		const string client_address = "0x22d491bde2303f2f43325b2108d26f1eaba1e32b";
+   		const string client_privkey = "6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c";
+
+
+   		auto is_syncing = co_await endpoint("eth_syncing", {});
+
+   		std::cout << "result: \n" << is_syncing << std::endl;
+
+   		if (is_syncing != false) {
+   			std::cout << "Host unavailable/not working" << std::endl;
+   			co_return 3;
+   		}
+
+
+   		string test_contract_bin = "6060604052341561000c57fe5b5b6101598061001c6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063cfae32171461003b575bfe5b341561004357fe5b61004b6100d4565b604051808060200182810382528381815181526020019150805190602001908083836000831461009a575b80518252602083111561009a57602082019150602081019050602083039250610076565b505050905090810190601f1680156100c65780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b6100dc610119565b604060405190810160405280600381526020017f486921000000000000000000000000000000000000000000000000000000000081525090505b90565b6020604051908101604052806000815250905600a165627a7a72305820ed71008611bb64338581c5758f96e31ac3b0c57e1d8de028b72f0b8173ff93a10029";
+        auto test_contract_addr  = co_await endpoint("eth_sendTransaction", {Map{{"from",uint256_t(orchid_address)},{"data",test_contract_bin}, {"gas","300000"}, {"gasPrice","0"}}} );
+
+
+   		string_to_file(test_contract_bin, "test_contract.bin");
+   		string test_contract_bin2 = file_to_string("test_contract.bin");
+        auto test_contract_addr2  = co_await endpoint("eth_sendTransaction", {Map{{"from",uint256_t(orchid_address)},{"data",test_contract_bin2}, {"gas","300000"}, {"gasPrice","0"}}} );
+
+
+   		 string OrchidToken_bin = file_to_string("tok-ethereum/build/OrchidToken.bin");
+   		//std::cout << OrchidToken_bin << std::endl;
+   		assert(OrchidToken_bin.size() > 0);
+        auto OrchidToken_addr = co_await endpoint("eth_sendTransaction", {Map{{"from",uint256_t(orchid_address)},{"data",OrchidToken_bin}, {"gas","300000"}, {"gasPrice","0"}}} );
+
+
+        co_return 0;
     }
 
 
@@ -73,8 +166,8 @@ namespace orc {
 
     void test_mpay()
     {
-        std::cout << "testing micropay!" << std::endl;
-        auto t = test_mpay_();
+        std::cout << "test_mpay()" << std::endl;
+        auto t = test_lottery();
         sync_wait(t);        
         std::cout << "test_mpay(): done" << std::endl;
     }
