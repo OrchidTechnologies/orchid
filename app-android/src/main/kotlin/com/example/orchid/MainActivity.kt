6e1e602aafe244b8928fa517bcf0f178b6d9926d
Jay Freeman (saurik) <saurik@saurik.com>
2019-06-05 03:15:27 -0700

    Add Flutter MethodChannel, in Kotlin, for Android.

--- app-android/src/main/kotlin/com/example/orchid/MainActivity.kt
+++ app-android/src/main/kotlin/com/example/orchid/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.orchid
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity(): FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
+
+        MethodChannel(flutterView, "orchid.com/feedback").setMethodCallHandler { call, result ->
+            when (call.method) {
+                "connect" -> {
+                    System.exit(-1)
+                }
+                "disconnect" -> {
+                }
+                "reroute" -> {
+                }
+            }
+        }
     }
 }
