f155b6003febfade3706e4a67c860a063da277ab
Greg Hazel <ghazel@gmail.com>
2019-07-17 16:03:56 -0700

    pass the file contents, not the filename. and, hold on to the capture_

--- vpn-android/source/orchid.cpp
+++ vpn-android/source/orchid.cpp
@@ -6,6 +6,9 @@
 #include <jni.h>
 #include <android/log.h>
 
+#include <fstream>
+#include <string>
+
 #include "transport.hpp"
 #include "file.hpp"
 
@@ -19,6 +22,7 @@ using namespace orc;
 static JavaVM *g_jvm;
 static asio::io_context *executor_;
 std::string files_dir;
+U<Sink<Capture>> capture_;
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_example_orchid_OrchidNative_runTunnel(JNIEnv* env, jobject thiz, jint file, jstring dir)
@@ -33,7 +37,10 @@ Java_com_example_orchid_OrchidNative_runTunnel(JNIEnv* env, jobject thiz, jint f
     files_dir = std::string(cDir);
     env->ReleaseStringUTFChars(dir, cDir);
 
-    std::string ovpnfile = files_dir + std::string("/PureVPN.ovpn");
+    std::string ovpnfilename = files_dir + std::string("/PureVPN.ovpn");
+    Log() << ovpnfilename << std::endl;
+    std::ifstream ifs(ovpnfilename);
+    std::string ovpnfile((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
     std::string username(ORCHID_USERNAME);
     std::string password(ORCHID_PASSWORD);
 
@@ -51,6 +58,7 @@ Java_com_example_orchid_OrchidNative_runTunnel(JNIEnv* env, jobject thiz, jint f
     ]() mutable -> task<void> { try {
         co_await Schedule();
         co_await capture->Start(std::move(ovpnfile), std::move(username), std::move(password));
+        capture_ = std::move(capture);
     } ORC_CATCH() });
     executor_ = &executor;
     executor_->run();
@@ -76,9 +84,9 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved)
 
 bool vpn_protect(int s)
 {
-    Log() << "vpn_protect: " << s << std::endl;
+    //Log() << "vpn_protect: " << s << std::endl;
     return boost::asio::post(*executor_, std::packaged_task<bool()>([&]{
-        Log() << "vpn_protect_inner: " << s << std::endl;
+        //Log() << "vpn_protect_inner: " << s << std::endl;
         JNIEnv *env;
         if (g_jvm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
             return false;
@@ -89,7 +97,7 @@ bool vpn_protect(int s)
         CATCH(return false);
         jboolean success = env->CallStaticBooleanMethod(cOrchidVpnService, mVpnProtect, s);
         CATCH(return false);
-        Log() << "vpn_protect fd:" << s << " success:" << (bool)success << std::endl;
+        //Log() << "vpn_protect fd:" << s << " success:" << (bool)success << std::endl;
         return (bool)success;
     })).get();
 }
