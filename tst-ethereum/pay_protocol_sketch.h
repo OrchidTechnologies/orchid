04aee0973060b72cc33d043df9b756ae60835d50
Jake <jake@orchid.com>
2019-08-06 17:23:17 -0700

    update after jay+justin chat, server invoicing sketch

--- tst-ethereum/pay_protocol_sketch.h
+++ tst-ethereum/pay_protocol_sketch.h
@@ -29,8 +29,8 @@ struct Client
 {
 
 	// budgeting params (input from UI)
-	budget_time_;   		// remaining time we want our balance to last
-	target_overhead_ = 0.1;	// target transaction fee overhead
+	budget_time_;   	// remaining time we want our balance to last
+	target_overhead_ = 0.1;	// target transaction fee overhead  // move to server?
 
 	// eth/system inputs (from some oracle)
 	trans_cost_;	// in OXT
@@ -41,50 +41,91 @@ struct Client
 
 	server_;
 	elapsed_;
-	balance_; 		// pot balance (shadowed)
+	totbytes_;
+	balance_; 		// actual pot balance (shadowed)
+
+	expBalance_;		// expected, micro-payment balance (amount owed from server to client)
+	minBalance_;		// min expected balance, below which we send a new micropayment
 
 	on_connect(address);  	// user initiates connection to server
 	on_connected(server);	// server connection response
-	on_winner(payment);		// server notifies client on winning ticket (needed for rate control)
+	on_winner(payment);	// server notifies client on winning ticket (needed for rate control)
+	on_in_packets(data);    // on inc data from server
+	on_out_packets(data);   // on out data to   server
 
+	GetAvgDataRate() {	// this could be smarter?  Avg data rate over all of time
+		return (totbytes_ / GetTotElapsedTime());
+	}
 
 	// user initiates connection to server
 	on_connect(server) 	 { send("connect", server, ...); }
 
+
+	get_budget_balance() { 
+		spendrate = balance_ / budget_time_; // simple dumb bad budgeting
+		allowed = spendrate * ElapsedTime();
+		return allowed;
+	};
+
 	// server connection response
 	on_connected(server)  {
-		server_ = server; balance_ = elapsed_ = 0;
+		server_ = server; balance_ = elapsed_ = expBalance_ = 0;
 
-		spendrate 		= balance_ / budget_time_;
-		upay_interval 	= 1.0 / server_.upay_freq_;
-		exp_value 		= spendrate * upay_interval;
+		afford 	    = get_budget_balance();
+		trust_bound = get_trust_bound();
+		exp_value   = min(afford, server_.exp_value_, trust_bound);
 
 		// init micropay params
-		faceVal_		= trans_cost_ / target_overhead_;
-		winProb_		= exp_value / faceVal_;
+		faceVal_	= trans_cost_ / target_overhead_; // todo: move to server
+		winProb_	= exp_value / (faceVal_ - trans_cost_);
 	}
 
 	// server notifies client on winning ticket (needed for rate control)
-	on_winner(payment)
-	{
-		balance_ 	   -= payment.faceVal;
-		spendrate 		= balance_ / budget_time_;
-		upay_interval 	= 1.0 / server_.upay_freq_;
-		exp_value 		= spendrate * upay_interval;
+	on_winner(payment)  {
+
+		balance_   -= payment.faceVal;
+
+		afford 	    = get_budget_balance();
+		trust_bound = get_trust_bound();
+		exp_value   = min(afford, server_.exp_value_, trust_bound);
 
 		// dyn adjust of faceVal rather than winprob (important)
-		faceVal_ 		= exp_value / winProb_;
+		faceVal_    = exp_value / winProb_; // todo: subject to server minFaceVal
 	}
+
+	on_in_packets(data)  {    // inc data from server
+		expBalance_ -= size(data) * server_.data_price_;
+		totbytes_   += size(data);
+	}
+
+	on_out_packets(data)  {    // out data to server
+		expBalance_ -= size(data) * server_.data_price_;
+		totbytes_   += size(data);
+	}
+
+	on_invoice(bal_owed) {
+		
+		afford 	    = get_budget_balance();
+		trust_bound = get_trust_bound();
+		exp_value   = min(afford, bal_owed, trust_bound);
+
+		// dyn adjust of faceVal rather than winprob (important)
+		faceVal_    = exp_value / winProb_; // todo: subject to server minFaceVal		
+		send("upay", server_, payment{winProb_, faceVal_, ..} );
+	}
+
 
 	update(etime)
 	{
-		elapsed_ 	 += etime;
+		elapsed_     += etime;
 		budget_time_ -= etime;
-		upay_interval = 1.0 / server_.upay_freq_;
 
-		while (elapsed_ > upay_interval) {
+		// todo: move to server, request
+		minBalance_  = server_.bytesPerUpay_ * (server_.data_price_);
+
+		if (expBalance_ < minBalance_) {
 			send("upay", server_, payment{winProb_, faceVal_, ..} );
-			elapsed_ -= upay_interval;
+			expBalance_ += winProb_ * faceVal_;
 		}
 	}
 
@@ -94,7 +135,8 @@ struct Client
 struct Server
 {
 	// Server Hyperparams - from a config file or something
-	upay_freq_;		// desired frequency of micropayments, in hertz
+	// upay_freq_;	// desired frequency of micropayments, in hertz
+	bytesPerUpay_;  // desired inv frequency of micropayments, in bytes
 	data_price_;	// server's OXT/byte price
 
 	// Internal
@@ -103,19 +145,19 @@ struct Server
 	oconn_map_;
 
 
-	on_connect(client);			// client connection request
+	on_connect(client);		// client connection request
 	on_upay(client, payment);	// upayment from client
 	on_out_packets(src, data);	// inc data from client out to target
-	on_in_packets(src, data);   // inc data from target out to client
+	on_in_packets(src, data);       // inc data from target out to client
 
 
 	// client connection request
 	on_connect(client) {
-		send("connected", client, {upay_freq_, ...} );
+		send("connected", client, { bytesPerUpay_, data_price_ ... } );
 	}
 
 	OnlinePaymentCheck(payment) { // validate the micropayment (does not verify it's a winner)
-		if (RPC.balance(payment.sender).amount_ < 	  payment.faceVal)) return false;
+		if (RPC.balance(payment.sender).amount_ <     payment.faceVal)) return false;
 		if (RPC.balance(payment.sender).escrow_ < 2 * payment.faceVal)) return false;
 	}
 
