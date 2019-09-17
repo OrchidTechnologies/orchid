4bf1e50979c601fbe6d29ff57c7dcff501f22346
Patrick Niemeyer <pat@pat.net>
2019-09-17 14:58:36 -0500

    Traffic detail view respects screen safe area.

--- app-shared/lib/pages/monitoring/traffic_view_detail.dart
+++ app-shared/lib/pages/monitoring/traffic_view_detail.dart
@@ -31,37 +31,39 @@ class _TrafficViewDetailState extends State<TrafficViewDetail> {
         DateFormat("MM/dd/yyyy HH:mm:ss.SSS").format(flow.start.toLocal());
     return TitledPage(
         title: "Connection Detail",
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: TrafficView.colorForProtocol(flow.protocol),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: TrafficView.colorForProtocol(flow.protocol),
+                  ),
+                  child: Text("${flow.protocol}",
+                      style: AppText.textLabelStyle
+                          .copyWith(fontSize: 14.0, color: AppColors.rneutral_1)),
                 ),
-                child: Text("${flow.protocol}",
-                    style: AppText.textLabelStyle
-                        .copyWith(fontSize: 14.0, color: AppColors.rneutral_1)),
-              ),
-              SizedBox(height: 12),
-              Text("Host: $hostname",
-                  // Note: I'd prefer ellipses but they brake soft wrap control.
-                  // Note: (Watch for the case of "-" dashes in domain names.)
-                  overflow: TextOverflow.fade,
-                  softWrap: false,
-                  style: AppText.textLabelStyle.copyWith(
-                      fontWeight: FontWeight.bold, color: AppColors.rneutral_1)),
-              SizedBox(height: 8),
-              Text("Time: $date", style: protStyle),
-              SizedBox(height: 2),
-              Text('Source Port : ${flow.src_port}', style: protStyle),
-              SizedBox(height: 2),
-              Text('Destination Addr: ${flow.dst_addr}', style: protStyle),
-              SizedBox(height: 2),
-              Text('Destination Port: ${flow.dst_port}', style: protStyle),
-            ],
+                SizedBox(height: 12),
+                Text("Host: $hostname",
+                    // Note: I'd prefer ellipses but they brake soft wrap control.
+                    // Note: (Watch for the case of "-" dashes in domain names.)
+                    overflow: TextOverflow.fade,
+                    softWrap: false,
+                    style: AppText.textLabelStyle.copyWith(
+                        fontWeight: FontWeight.bold, color: AppColors.rneutral_1)),
+                SizedBox(height: 8),
+                Text("Time: $date", style: protStyle),
+                SizedBox(height: 2),
+                Text('Source Port : ${flow.src_port}', style: protStyle),
+                SizedBox(height: 2),
+                Text('Destination Addr: ${flow.dst_addr}', style: protStyle),
+                SizedBox(height: 2),
+                Text('Destination Port: ${flow.dst_port}', style: protStyle),
+              ],
+            ),
           ),
         ));
   }
