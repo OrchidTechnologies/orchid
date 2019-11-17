e972f069590d5786e3d7a57cdcfed070544bcd52
Jay Freeman (saurik) <saurik@saurik.com>
2019-11-16 21:40:36 -0800

    Add file I'd forgotten in the previous commits :/.

---
+++ vpn-shared/source/heap.hpp
@@ -0,0 +1,79 @@
+/* Orchid - WebRTC P2P VPN Market (on Ethereum)
+ * Copyright (C) 2017-2019  The Orchid Authors
+*/
+
+/* GNU Affero General Public License, Version 3 {{{ */
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+/* }}} */
+
+
+#ifndef ORCHID_HEAP_HPP
+#define ORCHID_HEAP_HPP
+
+#include <duktape.h>
+
+namespace orc {
+
+class Heap {
+  private:
+    duk_context *duk_;
+
+  public:
+    Heap() :
+        duk_(duk_create_heap_default())
+    {
+    }
+
+    ~Heap() {
+        duk_destroy_heap(duk_);
+    }
+
+    operator duk_context *() {
+        return duk_;
+    }
+
+    template <typename Type_>
+    Type_ pop();
+
+    template <typename Type_>
+    Type_ eval(const std::string &code) {
+        duk_eval_string(duk_, code.c_str());
+        return pop<Type_>();
+    }
+};
+
+template <>
+inline void Heap::pop<void>() {
+    duk_pop(duk_);
+}
+
+template <>
+inline duk_double_t Heap::pop<duk_double_t>() {
+    auto value(duk_get_number(duk_, -1));
+    duk_pop(duk_);
+    return value;
+}
+
+template <>
+inline std::string Heap::pop<std::string>() {
+    std::string value(duk_get_string(duk_, -1));
+    duk_pop(duk_);
+    return value;
+}
+
+}
+
+#endif//ORCHID_HEAP_HPP
