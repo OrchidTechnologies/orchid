586c0e48b951f8805e57e0d3aded10d02246246e
Patrick Niemeyer <pat@pat.net>
2019-12-03 12:26:29 -0600

    Add hop instructions.

--- app-shared/lib/pages/circuit/add_hop_page.dart
+++ app-shared/lib/pages/circuit/add_hop_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:orchid/pages/app_text.dart';
 import 'package:orchid/pages/common/formatting.dart';
 import 'package:orchid/pages/common/titled_page_base.dart';
+import '../app_colors.dart';
 import 'circuit_hop.dart';
 import 'openvpn_hop_page.dart';
 import 'orchid_hop_page.dart';
@@ -21,7 +22,7 @@ class _AddHopPageState extends State<AddHopPage> {
   @override
   Widget build(BuildContext context) {
     return TitledPage(
-      title: "Add Circuit Hop",
+      title: "Select Hop Type",
       cancellable: true,
       backAction: () {
         widget.onAddFlowComplete(null);
@@ -34,6 +35,7 @@ class _AddHopPageState extends State<AddHopPage> {
             _divider(),
             _buildHopChoice(text: "OpenVPN Hop", hopType: Protocol.OpenVPN),
             _divider(),
+            Expanded(child: AddHopInstructions()),
           ],
         ),
       ),
@@ -80,4 +82,47 @@ class _AddHopPageState extends State<AddHopPage> {
 
   Divider _divider() =>
       Divider(color: Colors.black.withOpacity(0.3), height: 1.0);
+}
+
+class AddHopInstructions extends StatelessWidget {
+  final VoidCallback addHop;
+
+  const AddHopInstructions({Key key, this.addHop}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+      builder: (BuildContext context, Orientation orientation) {
+        return Visibility(
+          visible: orientation == Orientation.portrait,
+          child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Spacer(flex: 1),
+              Image.asset("assets/images/approach.png"),
+              SizedBox(height: 20),
+              AppText.header(
+                  text: "Choose your protocol",
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                  fontSize: 20.0),
+              SizedBox(height: 20),
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 450),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 45),
+                  child: AppText.body(
+                      text:
+                          "There are two types of hops to choose from. You can route your traffic through one of Orchid’s secure servers or you can use your existing VPN provider’s OpenVPN configuration.",
+                      fontSize: 15.0,
+                      color: AppColors.neutral_1),
+                ),
+              ),
+              Spacer(flex: 1),
+            ],
+          )),
+        );
+      },
+    );
+  }
 }
