7336c77c13054b860064c09338a340c152a1a0c5
Patrick Niemeyer <pat@pat.net>
2020-04-09 20:27:51 -0500

    Simplify PAC configuration and introduce small JS parser.

--- app-shared/lib/pages/onboarding/welcome_dialog.dart
+++ app-shared/lib/pages/onboarding/welcome_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:orchid/api/orchid_vpn_config.dart';
+import 'package:orchid/api/configuration/orchid_vpn_config.dart';
 import 'package:orchid/generated/l10n.dart';
 import 'package:orchid/pages/app_sizes.dart';
 import 'package:orchid/pages/app_text.dart';
