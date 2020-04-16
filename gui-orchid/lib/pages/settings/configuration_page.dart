97d6c37de620ee2240caefb12e94cc4e03abe0ef
Patrick Niemeyer <pat@pat.net>
2020-04-16 16:05:08 -0500

    App Ethereum provider is configurable with `ethProviderUrl` and temporarily defaults to Infura.

--- gui-orchid/lib/pages/settings/configuration_page.dart
+++ gui-orchid/lib/pages/settings/configuration_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:orchid/api/configuration/js_config.dart';
 import 'package:orchid/api/orchid_api.dart';
-import 'package:orchid/api/orchid_types.dart';
 import 'package:orchid/api/preferences/user_preferences.dart';
 import 'package:orchid/generated/l10n.dart';
 import 'package:orchid/pages/common/app_buttons.dart';
