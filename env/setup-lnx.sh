127b84d1722919659419894c6f67682ed00201c4
Jay Freeman (saurik) <saurik@saurik.com>
2020-04-25 15:26:06 -0700

    Always use -H on sudo so HOME is correct after -E.

--- env/setup-lnx.sh
+++ env/setup-lnx.sh
@@ -2,7 +2,7 @@
 set -e
 
 export DEBIAN_FRONTEND=noninteractive
-sudo -E apt-get -y install bc bison cpio flex gettext gperf groff ninja-build python python3-pip python3-setuptools rpm tcl
-sudo -E pip3 install meson
+sudo -EH apt-get -y install bc bison cpio flex gettext gperf groff ninja-build python python3-pip python3-setuptools rpm tcl
+sudo -EH pip3 install meson
 
 exec env/setup-all.sh
