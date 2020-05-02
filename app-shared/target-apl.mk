4760dabdc7aaf182712df6ab4563d53461d30825
Jay Freeman (saurik) <saurik@saurik.com>
2020-05-02 15:22:52 -0700

    Support a sane single-pass build of the macOS app.

--- app-shared/target-apl.mk
+++ app-shared/target-apl.mk
@@ -92,8 +92,13 @@ $(assemble)/Pods/Manifest.lock: $(assemble)/Podfile shared/gui/.flutter-plugins
 	cd $(assemble) && pod install
 	touch $@
 
-$(output)/XCBuildData/build.db: $(assemble)/Pods/Manifest.lock
+$(output)/XCBuildData/build.db: shared/empty.plist $(assemble)/Pods/Manifest.lock
 	xcodebuild -project $(assemble)/Pods/Pods.xcodeproj -alltargets -arch $(default) -sdk $(sdk) SYMROOT=$(CURDIR)/$(output)
+	for framework in $(output)/Release/*/*.framework; do \
+	    $(rsync) "$${framework}" "$(bundle)$(contents)/Frameworks"; \
+	    framework="$(bundle)$(contents)/Frameworks/$${framework##*/}"; \
+	    $(call codesign,$${framework},$<,$${framework}$(versions)$(signature)); \
+	done
 
 
 replace = sed -e 's/@MONOTONIC@/$(monotonic)/g; s/@VERSION@/$(version)/g; s/@REVISION@/$(revision)/g; s/@DOMAIN@/$(domain)/g; s/@NAME@/$(name)/g; s/@TEAMID@/$(teamid)/g; s/@SUPPORT@/$(support)/g' $< | if test -n "$(filter noaot,$(debug))"; then sed -e 's/^@D@/   /'; else sed -e '/^@D@/d'; fi | if $(beta); then sed -e 's/^@B@/   /'; else sed -e '/^@B@/d'; fi >$@
