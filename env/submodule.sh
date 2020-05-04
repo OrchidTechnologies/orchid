26b5d0ebaa7545e8d9bd2de5dcf9f26c64813d06
Jay Freeman (saurik) <saurik@saurik.com>
2020-05-04 13:56:13 -0700

    Add nettle to the list of broken git repositories.

--- env/submodule.sh
+++ env/submodule.sh
@@ -2,5 +2,6 @@
 set -e
 # XXX: flutter needs deep for an unknown reason; maybe this can be fixed?
 # XXX: the other repositories need uploadpack.allowReachableSHA1InWant :(
-git submodule update --init --recursive "$@" -- app-shared/flutter min-wireshark/{gnulib,libiconv} p2p/lwip vpn-shared/{libssh,tor}
+# XXX: nettle works if you use -c protocol.version=2, but I don't want to
+git submodule update --init --recursive "$@" -- app-shared/flutter min-wireshark/{gnulib,libiconv} p2p/{lwip,nettle} vpn-shared/{libssh,tor}
 git submodule update --init --recursive "$@" --depth 1
