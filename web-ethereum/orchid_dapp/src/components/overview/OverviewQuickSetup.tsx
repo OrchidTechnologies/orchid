532c6dc4436ba249959da83dc0ef29616b546e77
Patrick Niemeyer <pat@pat.net>
2020-05-14 14:10:00 -0500

    Change hardcoded dapp deposit and balance requirements in quick start.

--- web-ethereum/orchid_dapp/src/components/overview/OverviewQuickSetup.tsx
+++ web-ethereum/orchid_dapp/src/components/overview/OverviewQuickSetup.tsx
@@ -37,7 +37,6 @@ export const OverviewQuickSetup: React.FC<OverviewProps & OverviewQuickSetupProp
 
   const {setRoute, setNavEnabled} = useContext(RouteContext);
   const [walletBalance, setWalletBalance] = useState<BigInt | null>(null);
-  const [targetDeposit, setTargetDeposit] = useState<number | null>(null);
 
   // Create account state
   const [tx, setTx] = useState(initialTxStatus || new TransactionStatus());
@@ -48,12 +47,9 @@ export const OverviewQuickSetup: React.FC<OverviewProps & OverviewQuickSetupProp
   useEffect(() => {
     let api = OrchidAPI.shared();
     let walletSubscription = api.wallet_wait.subscribe(wallet => {
-      console.log("add funds got wallet: ", wallet);
+      console.log("quick setup funds got wallet: ", wallet);
       setWalletBalance(wallet.oxtBalance);
     });
-
-    // TODO: use live pricing: max(2 OXT, 2 USD)
-    setTargetDeposit(2.0); // OXT
 
     return () => {
       walletSubscription.unsubscribe();
@@ -64,7 +60,7 @@ export const OverviewQuickSetup: React.FC<OverviewProps & OverviewQuickSetupProp
   async function submitAddFunds() {
     let api = OrchidAPI.shared();
     let wallet = api.wallet.value;
-    if (wallet == null || targetDeposit == null || walletBalance == null) {
+    if (wallet == null || walletBalance == null) {
       return;
     }
     let walletAddress = wallet.address;
@@ -118,13 +114,14 @@ export const OverviewQuickSetup: React.FC<OverviewProps & OverviewQuickSetupProp
     }, 300);
   }
 
-  if (walletBalance == null || targetDeposit == null) {
+  if (walletBalance == null) {
     return <OverviewLoading/>
   }
 
   // TODO: Incorporate live pricing
-  let ticketFaceValue = 0.8; // TODO:
-  const targetBalance = 2 * ticketFaceValue;
+  // Determine required deposit and balance minimums
+  const targetDeposit = 15.0 // OXT
+  const targetBalance = 23.0 // OXT
   let sufficientFundsAmount = targetDeposit + targetBalance;
   let sufficientFunds = walletBalance >= oxtToKeiki(sufficientFundsAmount);
   let insufficientFundsText =
