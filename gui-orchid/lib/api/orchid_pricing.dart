c13adbf73a3bf874c2ac4d5627b59a017d7cddd9
Patrick Niemeyer <pat@pat.net>
2020-05-20 10:24:45 -0500

    Add logic to calculate ticket value based on market conditions.  Added "market stats" to Orchid Hop page.

---
+++ gui-orchid/lib/api/orchid_pricing.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+
+import 'package:orchid/api/orchid_budget_api.dart';
+import 'package:orchid/api/orchid_eth.dart';
+import 'package:orchid/util/units.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+/// Exchange rates
+class OrchidPricingAPI {
+  static OrchidPricingAPI _shared = OrchidPricingAPI._init();
+  static var exchangeRatesProviderUrl =
+      'https://api.coinbase.com/v2/exchange-rates';
+
+  OrchidPricingAPI._init();
+
+  factory OrchidPricingAPI() {
+    return _shared;
+  }
+
+  Pricing _lastPricing;
+
+  /// Get a snapshot of current pricing data at the current time.
+  /// This method may return null if no pricing data is available and the UI
+  /// should handle this as a routine condition by hiding displayed conversions.
+  Future<Pricing> getPricing() async {
+    // Cache for a period of time
+    if (_lastPricing != null &&
+        DateTime.now().difference(_lastPricing.date) < Duration(minutes: 5)) {
+      return _lastPricing;
+    }
+
+    try {
+      var response = await http.get(exchangeRatesProviderUrl);
+      if (response.statusCode != 200) {
+        throw Exception("Error status code: ${response.statusCode}");
+      }
+      var body = json.decode(response.body);
+      var rates = body['data']['rates'];
+      _lastPricing = Pricing(
+          ethToUsdRate: double.parse(rates['ETH']),
+          oxtToUsdRate: double.parse(rates['OXT']));
+      return _lastPricing;
+    } catch (err) {
+      print("Error fetching pricing: $err");
+      return null;
+    }
+  }
+
+  static int gasCostToRedeemTicket = 100000;
+
+  /// Calculate the current real world value of the largest ticket that can be
+  /// issued from this lottery pot, taking into account the amount of gas required
+  /// to redeem the ticket, current gas prices, and the OXT-ETH exchange rate.
+  /// Returns the net value in OXT, which may be zero or negative if the ticket
+  /// would be unprofitable to redeem.
+  Future<OXT> getMaxTicketValue(LotteryPot pot) async {
+    Pricing pricing = await getPricing();
+    GWEI gasPrice = await OrchidEthereum().getGasPrice();
+    ETH gasCostToRedeem = (gasPrice * gasCostToRedeemTicket).toETH();
+    OXT oxtCostToRedeem = pricing.ethToOxt(gasCostToRedeem);
+    OXT maxFaceValue = OXT.min(pot.balance, pot.deposit/ 2.0);
+    return maxFaceValue - oxtCostToRedeem;
+  }
+}
+
+/// Pricing captures exchange rates at a point in time and supports conversion.
+class Pricing {
+  DateTime date;
+  double ethToUsdRate;
+  double oxtToUsdRate;
+
+  Pricing({
+    DateTime date,
+    @required double ethToUsdRate,
+    @required double oxtToUsdRate,
+  }) {
+    this.date = date ?? DateTime.now();
+    this.ethToUsdRate = ethToUsdRate;
+    this.oxtToUsdRate = oxtToUsdRate;
+  }
+
+  USD toUSD(OXT oxt) {
+    if (oxt == null) {
+      return null;
+    }
+    return USD(oxt.value * oxtToUsdRate);
+  }
+
+  OXT toOXT(USD usd) {
+    if (usd == null) {
+      return null;
+    }
+    return OXT(usd.value / oxtToUsdRate);
+  }
+
+  OXT ethToOxt(ETH eth) {
+    return OXT(oxtToUsdRate / ethToUsdRate * eth.value);
+  }
+
+  @override
+  String toString() {
+    return 'Pricing{date: $date, ethToUsdRate: $ethToUsdRate, oxtToUsdRate: $oxtToUsdRate}';
+  }
+}
--- gui-orchid/lib/util/units.dart
+++ gui-orchid/lib/util/units.dart
@@ -1,6 +1,9 @@
+import 'dart:math' as math;
+import 'package:intl/intl.dart';
 
 class ScalarValue<T extends num> {
   final T value;
+
   const ScalarValue(this.value);
 
   String toString() {
@@ -8,19 +11,91 @@ class ScalarValue<T extends num> {
   }
 
   // Note: This is *not* locale sensitive. See NumberFormat.
+  // @See formatCurrency()
   String toStringAsFixed(int len) {
     return value.toStringAsFixed(len);
   }
 
   bool operator ==(o) => o is ScalarValue<T> && o.value == value;
+
   int get hashCode => value.hashCode;
 }
 
 class OXT extends ScalarValue<double> {
   const OXT(double value) : super(value);
 
+  OXT operator *(double other) {
+    return OXT(value * other);
+  }
+
+  OXT operator /(double other) {
+    return OXT(value / other);
+  }
+
+  OXT operator -(OXT other) {
+    return OXT(value - other.value);
+  }
+
+  OXT operator +(OXT other) {
+    return OXT(value + other.value);
+  }
+
+//  static String format(num value, {int digits = 4}) {
+//    return formatCurrency(value, digits: digits, suffix: " OXT");
+//  }
+
+  static OXT min(OXT a, OXT b) {
+    return OXT(math.min(a.value, b.value));
+  }
+
   static OXT fromWei(BigInt oxtWei) {
     return OXT(oxtWei / BigInt.from(1e18));
+  }
+}
+
+class ETH extends ScalarValue<double> {
+  const ETH(double value) : super(value);
+
+  static ETH fromWei(BigInt wei) {
+    return ETH(wei / BigInt.from(1e18));
+  }
+
+  ETH operator *(double other) {
+    return ETH(value * other);
+  }
+
+  ETH operator /(double other) {
+    return ETH(value / other);
+  }
+
+  @override
+  String toString() {
+    return 'ETH{$value}';
+  }
+}
+
+class GWEI extends ScalarValue<double> {
+  const GWEI(double value) : super(value);
+
+  ETH toETH() {
+    return ETH(value / 1e9);
+  }
+
+  GWEI operator *(num other) {
+    return GWEI(value * other);
+  }
+
+  GWEI operator /(double other) {
+    return GWEI(value / other);
+  }
+
+  static GWEI fromWei(BigInt wei) {
+    return GWEI(wei / BigInt.from(1e9));
+  }
+
+  @override
+  String toString() {
+    return 'GWEI{$value}';
   }
 }
 
@@ -32,3 +107,13 @@ class Months extends ScalarValue<int> {
   const Months(int value) : super(value);
 }
 
+/// Format a currency to default two digits of precision with an optional suffix
+/// and null behavior.
+String formatCurrency(num value,
+    {String suffix, int digits = 2, String ifNull = "..."}) {
+  if (value == null) {
+    return ifNull;
+  }
+  return NumberFormat("#0.0" + "#" * (digits - 1)).format(value) +
+      (suffix != null ? " $suffix" : "");
+}
