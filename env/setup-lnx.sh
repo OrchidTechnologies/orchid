b82bbac4a0e690a4c10912af69c604d1ec5aa010
Jay Freeman (saurik) <saurik@saurik.com>
2020-05-26 04:15:59 -0700

    The docker.sh build was strangely/suddenly broken.

--- env/setup-lnx.sh
+++ env/setup-lnx.sh
@@ -2,7 +2,7 @@
 set -e
 
 export DEBIAN_FRONTEND=noninteractive
-sudo -EH apt-get -y install autoconf bc bison cpio capnproto flex gettext gperf groff libtool ninja-build python python3-pip python3-setuptools rpm tcl texinfo
+sudo -EH apt-get -y install autoconf bc bison cpio capnproto flex gettext gperf groff libtool ninja-build pkg-config python python3-pip python3-setuptools rpm tcl texinfo
 sudo -EH apt-get -y install clang-9 libc++-9-dev libc++abi-9-dev clang-tidy-9
 
 function usable() {
