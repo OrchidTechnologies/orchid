e31704ec8a431e2faa385c5ef0c0f19a2b31f595
Jay Freeman (saurik) <saurik@saurik.com>
2020-06-01 02:37:52 -0700

    Drop file from v8 Torque breaking Windows link :(.

--- min-v8/target.mk
+++ min-v8/target.mk
@@ -129,7 +129,6 @@ $(output)/$(pwd/v8)/torque: $(wildcard $(pwd)/v8/src/torque/*.cc) $(pwd)/v8/src/
 	clang++ -std=c++14 -pthread -o $@ $^ $(vflags)
 
 tqsrc := $(patsubst %.tq,%-tq-csa.cc,$(torque))
-tqsrc += class-debug-readers-tq.cc
 tqsrc += class-definitions-tq.cc
 tqsrc += class-verifiers-tq.cc
 tqsrc += exported-macros-assembler-tq.cc
