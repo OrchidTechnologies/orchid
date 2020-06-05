b64a1c63217cb98b8412af23b3bafbcd1e081bda
Jay Freeman (saurik) <saurik@saurik.com>
2020-06-05 08:54:39 +0000

    Use RUSTC_WRAPPER hack to make Cargo reproducible.

--- env/output.mk
+++ env/output.mk
@@ -201,8 +201,9 @@ $(output)/%.rustup:
 $(output)/%/librust.a: $$(specific) $$(folder)/Cargo.toml $(output)/$$(triple/$$(arch)).rustup $(sysroot) $$(call head,$$(folder))
 	$(specific)
 	@mkdir -p $(dir $@)
-	cd $(folder) && RUST_BACKTRACE=1 PATH=$${PATH}:$(dir $(word 1,$(cc))) $(if $(ccrs/$(arch)),$(ccrs/$(arch)),TARGET)_CC='$(cc) $(more/$(arch)) $(qflags)' \
-	    PKG_CONFIG_ALLOW_CROSS=1 PKG_CONFIG="$(CURDIR)/env/pkg-config" ENV_ARCH="$(arch)" AR='$(ar/$(arch))' \
+	cd $(folder) && RUST_BACKTRACE=1 RUSTC_WRAPPER=$(CURDIR)/env/rustc-wrapper PATH=$${PATH}:$(dir $(word 1,$(cc))) \
+	    $(if $(ccrs/$(arch)),$(ccrs/$(arch)),TARGET)_CC='$(cc) $(more/$(arch)) $(qflags)' AR='$(ar/$(arch))' \
+	    PKG_CONFIG_ALLOW_CROSS=1 PKG_CONFIG="$(CURDIR)/env/pkg-config" ENV_ARCH="$(arch)" \
 	    CARGO_HOME='$(CURDIR)/$(output)/cargo' cargo build --verbose --lib --release \
 	    --target $(triple/$(arch)) --target-dir $(CURDIR)/$(output)/$(arch)/$(folder)
 	cp -f $(output)/$(arch)/$(folder)/$(triple/$(arch))/release/deps/lib$(subst -,_,$(notdir $(folder))).a $@
