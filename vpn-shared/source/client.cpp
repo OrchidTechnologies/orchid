e1f9d68c9011274b101c4d8b8b58db3cc0d4ffeb
Jay Freeman (saurik) <saurik@saurik.com>
2020-06-23 12:38:32 -0700

    Quick hack to ring() as returns (...) is tuple? :/

--- vpn-shared/source/client.cpp
+++ vpn-shared/source/client.cpp
@@ -98,9 +98,9 @@ void Client::Transfer(size_t size) {
 cppcoro::shared_task<Bytes> Client::Ring(Address recipient) {
     if (seller_ == Address(0))
         co_return Bytes();
-    static const Selector<Bytes, Bytes, Address> ring_("ring");
+    static const Selector<std::tuple<Bytes>, Bytes, Address> ring_("ring");
     static const std::string latest("latest");
-    co_return co_await ring_.Call(endpoint_, latest, seller_, 90000, shared_, recipient);
+    co_return std::get<0>(co_await ring_.Call(endpoint_, latest, seller_, 90000, shared_, recipient));
 }
 
 void Client::Land(Pipe *pipe, const Buffer &data) {
