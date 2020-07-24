04a7e034aa19071c6ad01ba13c836f35ac2e0a30
Nathan Handler <nhandler@orchid.com>
2020-07-24 13:50:47 -0700

    Report status/viability of individual PAC tiers

--- tst-ethereum/pac/storestatus.py
+++ tst-ethereum/pac/storestatus.py
@@ -3,9 +3,24 @@ import logging
 import os
 
 from utils import configure_logging
+from utils import get_product_id_mapping
 
 
 configure_logging()
+
+
+def is_pac_tier_viable(price: str) -> bool:
+    logging.debug(f'is_pac_tier_viable(): price: {price}')
+    return True
+
+
+def get_tier_statuses():
+    mapping = get_product_id_mapping()
+    statuses = {}
+    for tier in mapping:
+        usd = mapping[tier]
+        statuses[tier] = is_pac_tier_viable(price=usd)
+    return statuses
 
 
 def main(event, context):
@@ -13,16 +28,24 @@ def main(event, context):
 
     logging.debug(f'store status stage:{stage}')
     logging.debug(f'event: {event}')
-    logging.debug(f'context: {context}')
+
+    statuses = get_tier_statuses()
+
+    disabled = False
+    status = 'Healthy'
+
+    body = {
+        'status': status,
+        'disabled': disabled,
+    }
+
+    body.update(statuses)
 
     response = {
         "isBase64Encoded": False,
         "statusCode": 200,
         "headers": {},
-        "body": json.dumps({
-            'status': 'Healthy',
-            'disabled': False,
-        })
+        "body": json.dumps(body)
     }
     logging.debug(f'storestatus response: {response}')
     return response
