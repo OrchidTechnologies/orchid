4c5cc4c2ef3130af05bfb6892a03c5c3ad765b09
Nathan Handler <nhandler@orchid.com>
2020-07-29 13:13:43 -0700

    Update PAC pricing

--- tst-ethereum/pac/utils.py
+++ tst-ethereum/pac/utils.py
@@ -53,9 +53,9 @@ def get_min_escrow():
 def get_product_id_mapping(store: str = 'apple') -> dict:
     mapping = {}
     mapping['apple'] = {
-        'net.orchid.pactier1': 12.99,
-        'net.orchid.pactier2': 29.99,
-        'net.orchid.pactier3': 79.99,
+        'net.orchid.pactier1': 39.99,
+        'net.orchid.pactier2': 79.99,
+        'net.orchid.pactier3': 199.99,
     }
 #     mapping['google'] = {
 #         'net.orchid.pactier1': 4.99,
