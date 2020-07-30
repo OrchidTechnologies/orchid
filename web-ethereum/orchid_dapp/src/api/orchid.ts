c46cc35ba8ceb6070d12e59be530a5aca6fae38c
Patrick Niemeyer <pat@pat.net>
2020-07-30 16:48:42 -0500

    Change recommended balance and deposit in the dapp.

--- web-ethereum/orchid_dapp/src/api/orchid.ts
+++ web-ethereum/orchid_dapp/src/api/orchid.ts
@@ -1,6 +1,6 @@
 import {OXT} from "./orchid-types";
 
 export class Orchid {
-  static recommendedBalance = new OXT(23);
-  static recommendedDeposit = new OXT(15);
+  static recommendedBalance = new OXT(100);
+  static recommendedDeposit = new OXT(50);
 }
