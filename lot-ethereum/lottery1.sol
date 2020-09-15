75a805e548b4654dfda2226fcfeb81b483eab2b6
Jay Freeman (saurik) <saurik@saurik.com>
2020-09-15 05:34:44 -0700

    Reorganize ticket encoding to match Ticket struct.

--- lot-ethereum/lottery1.sol
+++ lot-ethereum/lottery1.sol
@@ -153,7 +153,7 @@ contract OrchidLottery1 {
 
         bytes32 digest; assembly { digest := chainid() } digest = keccak256(abi.encode(
             keccak256(abi.encodePacked(keccak256(abi.encodePacked(ticket.reveal)), ticket.salt, recipient)),
-            ticket.issued, ticket.nonce, this, digest, amount, ratio, ticket.start, range, funder));
+            this, digest, ticket.issued, ticket.nonce, ticket.start, range, amount, ratio, funder));
         signer = ecrecover(digest, ticket.v, ticket.r, ticket.s);
 
     {
