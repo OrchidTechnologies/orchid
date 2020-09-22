98fd68cc6647e6e5d43d8e6916892e8259763e40
Jay Freeman (saurik) <saurik@saurik.com>
2020-09-22 01:50:04 -0700

    I failed to add the verifier code for PAC test :/.

---
+++ lot-ethereum/verifier.sol
@@ -0,0 +1,36 @@
+/* Orchid - WebRTC P2P VPN Market (on Ethereum)
+ * Copyright (C) 2017-2019  The Orchid Authors
+*/
+
+/* GNU Affero General Public License, Version 3 {{{ */
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+/* }}} */
+
+
+pragma solidity 0.7.1;
+
+import "./include.sol";
+
+contract OrchidPassword is OrchidVerifier {
+    function book(bytes memory, address, bytes memory receipt) override public pure {
+        require(keccak256(receipt) == 0xb68fe43f0d1a0d7aef123722670be50268e15365401c442f8806ef83b612976b);
+    }
+
+    function ring(bytes calldata, address) override external pure returns (bytes memory) {
+        return "";
+    }
+}
