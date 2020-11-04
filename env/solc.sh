19d2a70de9c1059172873e3dca0d0c227f5fcbce
Jay Freeman (saurik) <saurik@saurik.com>
2020-11-04 11:13:01 +0000

    Remove metadata hashes from Solidity contracts :/.

--- env/solc.sh
+++ env/solc.sh
@@ -12,6 +12,10 @@ if [[ ${output} != - ]]; then
     flags+=(-o /mnt/"${PWD##*/}"/"${output}")
 fi
 
+if dpkg --compare-versions "${version}" '>=' 0.6.0; then
+    flags+=(--metadata-hash none)
+fi
+
 exec docker run -v "${cwd%/*}":/mnt ethereum/solc:"${version}" \
     "${flags[@]}" /mnt/"${PWD##*/}"/"${source}" \
     --bin --overwrite --allow-paths . "$@"
