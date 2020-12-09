2daa71266ea47a734dadf015a677bd75a2083aed
jake cannell <jake.cannell@gmail.com>
2020-12-08 16:57:48 -0800

    pacs: new sendRaw interface (incomplete)

--- pac-serverless/w3_generic.py
+++ pac-serverless/w3_generic.py
@@ -28,17 +28,28 @@ def get_usd_per_x_binance(token_sym) -> float:
     logging.debug(f"usd_per_x_binance {token_sym}: {usd_per_eth}")
     return usd_per_x
 
-def sendRaw_wei(web3_websock,txn,  pubkey,privkey,nonce,max_cost_wei):
+def get_txn_cost_wei(txn):
     value_wei    = txn['value']
     gas          = txn['gas']
     gasPrice     = txn['gasPrice']
     gascost_wei  = gas*gasPrice
     cost_wei     = value_wei + gascost_wei
-    #value_usd    = wei_to_usd(value_wei + gascost_wei)
+    return cost_wei
 
+wei_per_eth = 1000000000000000000
+
+def get_txn_cost_usd(txn):
+    cost_wei  = get_txn_cost_wei(txn)
+    cost_eth  = cost_wei / wei_per_eth
+    cost_usd  = cost_eth * usd_per_eth
+    return cost_usd
+
+def sendRaw_wei_(web3_websock,txn,  pubkey,privkey,nonce,max_cost_wei):
+
+    cost_wei     = get_txn_cost_wei(txn)
     if (cost_wei > max_cost_wei):
         logging.debug(f'sign_send_Transaction cost_wei({cost_wei}) > max_cost_wei({max_cost_wei})')
-        return None
+        return None,0.0
 
     w3       = Web3(Web3.WebsocketProvider(web3_websock, websocket_timeout=900))
 
@@ -50,12 +61,10 @@ def sendRaw_wei(web3_websock,txn,  pubkey,privkey,nonce,max_cost_wei):
 
     txn_hash = w3.eth.sendRawTransaction(txn_signed.rawTransaction)
     logging.debug(f'sign_send_Transaction submitted transaction with hash: {txn_hash.hex()}')
-    return txn_hash.hex()
+    return txn_hash.hex(),cost_wei
 
 
-wei_per_eth = 1000000000000000000
-
-def sendRaw_usd(web3_websock,txn,  pubkey,privkey,nonce,max_cost_usd):
+def sendRaw_usd_(web3_websock,txn, pubkey,privkey,nonce,max_cost_usd):
 
     usd_per_eth = get_usd_per_x_coinbase('ETH')
     if (usd_per_eth == 0.0):
@@ -64,7 +73,63 @@ def sendRaw_usd(web3_websock,txn,  pubkey,privkey,nonce,max_cost_usd):
     max_cost_eth = max_cost_usd / usd_per_eth
     max_cost_wei = max_cost_eth * wei_per_eth
 
-    return sendRaw_wei(web3_websock,txn, pubkey,privkey,nonce,max_cost_wei)
+    txnhash,cost_wei = sendRaw_wei_(web3_websock,txn, pubkey,privkey,nonce,max_cost_wei)
+
+    cost_eth = cost_wei / wei_per_eth
+    cost_usd = cost_eth * usd_per_eth
+
+    return txnhash,cost_usd
+
+
+def sendRaw(web3_websock,txn,receiptHash):
+
+    #todo: make fully idempotent, get the transaction hash and look it up?
+
+    max_cost_usd     = get_account_balance(receiptHash)
+    pubkey,privkey   = get_executor_account()
+    nonce            = get_nonce(pubkey)
+
+    txnhash,cost_usd = sendRaw_usd_(web3_websock,txn, pubkey,privkey,nonce,max_cost_usd)
+
+    if (txnhash != None):
+        save_transaction(txnhash, txn)
+        debit_account_balance(receiptHash, cost_usd)
+
+    return txnhash
+
+
+def has_transaction_failed(web3_websock,txnhash,txn):
+
+    w3 = Web3(Web3.WebsocketProvider(web3_websock, websocket_timeout=900))
+    success     = True
+    txn_receipt = None
+    try:
+        txn_receipt = w3.eth.getTransactionReceipt(txnhash)
+        success     = Bool(txn_receipt['status'])
+    except (Web3.exceptions.TransactionNotFound, TransactionNotFound):
+        txn_receipt = None
+
+    pubkey     = txn['from']
+    txn_nonce  = txn['nonce']
+    cur_nonce  = get_nonce(pubkey)
+
+    result = (success == False) or ((txn_receipt == None) and (cur_nonce > txn_nonce))
+
+    return result
+
+
+
+def refund_failed_txn(web3_websock,txnhash,receiptHash):
+
+    txn = load_transaction(txnhash)
+
+    if (has_transaction_failed(web3_websock,txn) == False):
+        return "error: transaction {} is still pending"
+
+    cost_usd = get_txn_cost_usd(txn)
+    credit_account_balance(receiptHash, cost_usd)
+
+    return "success"
 
 
 '''
