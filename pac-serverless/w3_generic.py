7240bb9338d293c80321923904fa024e9709e4c0
jake cannell <jake.cannell@gmail.com>
2021-01-14 09:51:29 -0800

    PACS: simple send_raw test working

--- pac-serverless/w3_generic.py
+++ pac-serverless/w3_generic.py
@@ -21,7 +21,7 @@ if (LocalTest == False):
     def dynamodb_readall(tableName):
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(tableName)
-        results = table.query()
+        results = table.scan()
         return results
 
     def dynamodb_read1(tableName, keyname, key):
@@ -54,7 +54,7 @@ if (LocalTest == False):
         if (item is None):
             item = {}
         else:
-            balance += item['balance']
+            balance += float(item['balance'])
         item['balance'] = balance
         item['receiptHash'] = receiptHash
         dynamodb_write1(os.environ['BALANCES_TABLE_NAME'], item)
@@ -65,7 +65,8 @@ if (LocalTest == False):
         item = dynamodb_read1(os.environ['BALANCES_TABLE_NAME'], 'receiptHash', receiptHash)
         balance = 0
         if (item is not None):
-            balance = item['balance'] = item['balance'] - cost_usd
+            balance = float(item['balance']) - cost_usd
+            item['balance'] = balance
             dynamodb_write1(os.environ['BALANCES_TABLE_NAME'], item)
         else:
             logging.debug(f"debit_account_balance account {receiptHash} not found")
@@ -83,7 +84,7 @@ if (LocalTest == False):
     def get_executor_account():
         results = dynamodb_readall(os.environ['EXECUTORS_TABLE_NAME'])
         num_execs = results['Count']
-        index = randit(0,num_execs-1)
+        index = random.randint(0,num_execs-1)
         pubkey = None
         privkey = None
         if (num_execs > 0):
@@ -163,53 +164,46 @@ def get_txn_cost_wei(txn):
     value_wei    = txn['value']
     gas          = txn['gas']
     gasPrice     = txn['gasPrice']
-    gascost_wei  = gas*gasPrice
-    cost_wei     = value_wei + gascost_wei
+    gascost_wei  = int(gas,0)*int(gasPrice,0)
+    cost_wei     = int(value_wei,0) + int(gascost_wei)
+    logging.debug(f"get_txn_cost_wei gas({gas}) gasPrice({gasPrice}) gascost_wei({gascost_wei}) cost_wei({cost_wei})")
     return cost_wei
 
 wei_per_eth = 1000000000000000000
 
-'''
-def get_txn_cost_usd(txn, symbol):
-    cost_wei  = get_txn_cost_wei(txn)
-    cost_eth  = cost_wei / wei_per_eth
-    usd_per_x = get_usd_per_x_binance(symbol)
-    if (usd_per_x == 0.0):
-        usd_per_x = get_usd_per_x_coinbase(symbol)
-    cost_usd  = cost_eth * usd_per_x
-    return cost_usd
-'''
 
-def sendRaw_wei_(w3,txn,  pubkey,privkey,nonce,max_cost_wei):
+def send_raw_wei_(w3,txn,  pubkey,privkey,nonce,max_cost_wei):
 
-    cost_wei     = get_txn_cost_wei(txn)
-    if (cost_wei > max_cost_wei):
-        logging.debug(f'sign_send_Transaction cost_wei({cost_wei}) > max_cost_wei({max_cost_wei})')
-        return None,0.0
-
-    #w3       = Web3(Web3.WebsocketProvider(W3WSock, websocket_timeout=900))
+    cost_wei     = float(get_txn_cost_wei(txn))
 
     txn['from']  = pubkey
     txn['nonce'] = nonce
 
-    txn_signed = w3.eth.account.sign_transaction(txn, private_key=privkey,)
+    logging.debug(f'send_raw_wei_ from({pubkey}) nonce({nonce}) cost_wei({cost_wei}) max_cost_wei({max_cost_wei})')
+
+    if (cost_wei > max_cost_wei):
+        logging.debug(f'sign_send_Transaction cost_wei({cost_wei}) > max_cost_wei({max_cost_wei})')
+        return None,0.0
+
+    txn_signed = w3.eth.account.sign_transaction(txn, private_key=privkey)
     logging.debug(f'sign_send_Transaction txn_signed: {txn_signed}')
 
     txn_hash = w3.eth.sendRawTransaction(txn_signed.rawTransaction)
     logging.debug(f'sign_send_Transaction submitted transaction with hash: {txn_hash.hex()}')
+
     return txn_hash.hex(),cost_wei
 
 
-def sendRaw_usd_(w3,txn, pubkey,privkey,nonce,max_cost_usd):
+def send_raw_usd_(w3,txn, pubkey,privkey,nonce,max_cost_usd):
 
     usd_per_eth = get_usd_per_x_coinbase('ETH')
     if (usd_per_eth == 0.0):
         usd_per_eth = get_usd_per_x_binance('ETH')
 
-    max_cost_eth = max_cost_usd / usd_per_eth
-    max_cost_wei = max_cost_eth * wei_per_eth
+    max_cost_eth = float(max_cost_usd) / float(usd_per_eth)
+    max_cost_wei = float(max_cost_eth) * float(wei_per_eth)
 
-    txnhash,cost_wei = sendRaw_wei_(w3,txn, pubkey,privkey,nonce,max_cost_wei)
+    txnhash,cost_wei = send_raw_wei_(w3,txn, pubkey,privkey,nonce,max_cost_wei)
 
     cost_eth = cost_wei / wei_per_eth
     cost_usd = cost_eth * usd_per_eth
@@ -221,10 +215,11 @@ def get_nonce_(w3,pubkey):
     return nonce
 
 
-def sendRaw(W3WSock,txn,receiptHash):
+def send_raw(W3WSock,txn,receiptHash):
 
-    if (target_in_whitelist(txn['to']) == False):
-        errmsg = f'ERROR sendRaw: txn target: {txn} non in whitelist'
+    to = txn['to']
+    if (target_in_whitelist(to) == False):
+        errmsg = f'ERROR sendRaw: txn target: {to} not in whitelist'
         logging.debug(errmsg)
         return None,0,errmsg
 
@@ -234,7 +229,9 @@ def sendRaw(W3WSock,txn,receiptHash):
     pubkey,privkey   = get_executor_account()
     nonce            = get_nonce_(w3,pubkey)
 
-    txnhash,cost_usd = sendRaw_usd_(w3,txn, pubkey,privkey,nonce,max_cost_usd)
+    logging.debug(f'send_raw max_cost_usd({max_cost_usd}) pubkey({pubkey}) privkey({privkey}) nonce({nonce}) ')
+
+    txnhash,cost_usd = send_raw_usd_(w3,txn, pubkey,privkey,nonce,max_cost_usd)
 
     msg = "unknown error"
     if (txnhash is not None):
@@ -292,31 +289,3 @@ def refund_failed_txn(W3WSock,txnhash,receiptHash):
     save_transaction(txn,txnhash)
 
     return "success"
-
-
-'''
-def build_sign_send_Transaction(web3_websock,contract_addr,func_name,args,value,gas,gasprice,  pubkey,privkey,nonce,max_cost_usd):
-    #w3       = Web3(Web3.WebsocketProvider(os.environ['WEB3_WEBSOCKET'], websocket_timeout=900))
-    w3       = Web3(Web3.WebsocketProvider(web3_websock, websocket_timeout=900))
-    contract = w3.eth.contract( abi=get_contract_abi(contract_addr), address=contract_addr, )
-    #method   = getattr(contract.functions, func_name)
-    method   = contract.get_function_by_name(func_name)
-
-    txn = method(**args).buildTransaction(
-        {
-            'chainId': 1,
-            'from': pubkey,
-            'gas': gas,
-            'gasPrice': gasprice,
-            'nonce': nonce,
-        }
-    )
-    logging.debug(f'build_sign_send_Transaction txn: {txn}')
-
-    txn_signed = w3.eth.account.sign_transaction(txn, private_key=privkey,)
-    logging.debug(f'build_sign_send_Transaction txn_signed: {txn_signed}')
-
-    txn_hash = w3.eth.sendRawTransaction(txn_signed.rawTransaction)
-    logging.debug(f'build_sign_send_Transaction submitted transaction with hash: {txn_hash.hex()}')
-    return txn_hash.hex()
-'''
