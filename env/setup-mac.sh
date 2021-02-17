71449c8c5be2ee514e4e065727f495bf85c4363c
Jay Freeman (saurik) <saurik@saurik.com>
2021-02-17 13:31:07 -0800

    Improve rustup installation in macOS setup script.

--- env/setup-mac.sh
+++ env/setup-mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-brew install autoconf automake capnp libtool meson rpm zstd
+brew install autoconf automake capnp libtool meson rpm rustup-init zstd
+rustup-init -y --no-modify-path --no-update-default-toolchain
 env/setup-all.sh
