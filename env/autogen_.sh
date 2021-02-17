0a7ce59153c508dd9aa7db3a94c550bafb038078
Jay Freeman (saurik) <saurik@saurik.com>
2021-02-17 11:11:34 -0800

    Attempt (fail) to fix the docker autogen fallback.

--- env/autogen_.sh
+++ env/autogen_.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -y install autoconf libtool
+apt-get -y install autoconf gettext git-core libtool
 cd /mnt
 # XXX: run autogen__.sh
 NOCONFIGURE=1 ./autogen.sh
