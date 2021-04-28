22bd32e4c9829e7e24cfa7b43b5f0adcb0bc2a02
Jay Freeman (saurik) <saurik@saurik.com>
2021-04-28 21:07:55 +0000

    Upgrade to even newer v8 (they fixed my filings!).

--- min-v8/target.mk
+++ min-v8/target.mk
@@ -219,11 +219,9 @@ cflags += -I$(pwd)/extra
 #chacks/$(pwd/v8)/src/./profiler/heap-snapshot-generator.cc += s/V8_CC_MSVC/1/
 cflags += -mno-ms-bitfields
 
-# https://bugs.chromium.org/p/v8/issues/detail?id=11692
-cflags/$(pwd/v8)/src/./runtime/runtime-classes.cc += -Wno-unused-variable
-
-# XXX: https://bugs.chromium.org/p/v8/issues/detail?id=11691
-cflags/$(pwd/v8)/ += -Wno-implicit-int-float-conversion
+# https://bugs.chromium.org/p/chromium/issues/detail?id=989932
+cflags/$(pwd/v8)/src/./compiler/representation-change.cc += -Wno-implicit-int-float-conversion
+cflags/$(pwd/v8)/src/./wasm/wasm-external-refs.cc += -Wno-implicit-int-float-conversion
 
 # https://bugs.chromium.org/p/chromium/issues/detail?id=1016945
 cflags/$(pwd/v8)/ += -Wno-builtin-assume-aligned-alignment
