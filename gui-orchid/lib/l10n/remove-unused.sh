3785200a035f09183c429f7867e6edd2c2e50b28
Patrick Niemeyer <pat@pat.net>
2021-07-26 13:55:54 -0500

    app: Remove unused strings from localization files.

---
+++ gui-orchid/lib/l10n/remove-unused.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd $(dirname "$0")
+
+echo "finding unused strings..."
+filterfile=/tmp/unused.$$
+./unused.sh | sed 's/^/\"/; s/$/\":/' > $filterfile
+
+echo "removing..."
+for f in *.arb
+do
+    org="$f.org"
+    if [ -f $org ]; then echo "Error: File exists: $org"; exit; fi
+    cp $f $org
+    grep -v -f $filterfile $org > $f
+done
+
---
+++ gui-orchid/lib/l10n/unused.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Attempt to find unused strings in the generated localization file.
+#
+
+cd $(dirname "$0")
+
+gen=../../.dart_tool/flutter_gen/gen_l10n/app_localizations.dart 
+list=$(grep '^  String get' $gen | sed 's/^  String get //' | tr -d ';')
+cd ..
+for s in $list
+do
+    got=$(egrep --exclude-dir l10n -r "(S.of\(context\)|s)\.${s}([^A-Za-z0-9]|$)" .)
+    if [ -z "$got" ]; then echo "$s"; fi
+done
