7d3bbc6d411470652f27cd855dc59cf4d7ece493
Patrick Niemeyer <pat@pat.net>
2021-08-04 13:17:37 -0500

    app: Accept raw 64 char hex keys in Orchid account import.

--- gui-orchid/lib/api/configuration/orchid_account_config/orchid_account_v1.dart
+++ gui-orchid/lib/api/configuration/orchid_account_config/orchid_account_v1.dart
@@ -6,7 +6,6 @@ import 'orchid_account_v0.dart';
 
 /// Support for reading and generating the JavaScript configuration file used by the Orchid VPN.
 class OrchidAccountConfigV1 {
-
   /// Parse a V1 account identity config.
   // e.g. 'account = {secret:"0xfb5d5..."}'
   static ParseOrchidIdentityResult parseOrchidIdentity(
@@ -15,8 +14,29 @@ class OrchidAccountConfigV1 {
       var config = JSConfig(js);
       var secret = config.evalString('account.secret');
       var newKeys = <StoredEthereumKey>[]; // type required here
-      var signer =
-          OrchidAccountConfigV0.resolveImportedKey(secret, existingKeys, newKeys);
+      var signer = OrchidAccountConfigV0.resolveImportedKey(
+          secret, existingKeys, newKeys);
+      return ParseOrchidIdentityResult(signer, newKeys.isNotEmpty);
+    } catch (err) {
+      print("Error parsing orchid identity: $err");
+      throw err;
+    }
+  }
+
+  /// Parse a 64 character raw hex encoded key with leading zeroes if required.
+  /// The string may optionally be prefixed with "0x" making it 66 characters.
+  /// The string is trimmed and case is ignored.
+  static ParseOrchidIdentityResult parseRawKey(
+      String secretIn, List<StoredEthereumKey> existingKeys) {
+    var secret = secretIn.toLowerCase().trim();
+    RegExp hexEncodedSecret = RegExp(r'^(0x)?[a-f0-9]{64}$');
+    if (!hexEncodedSecret.hasMatch(secret)) {
+      throw Exception("Invalid key");
+    }
+    try {
+      var newKeys = <StoredEthereumKey>[];
+      var signer = OrchidAccountConfigV0.resolveImportedKey(
+          secret, existingKeys, newKeys);
       return ParseOrchidIdentityResult(signer, newKeys.isNotEmpty);
     } catch (err) {
       print("Error parsing orchid identity: $err");
@@ -55,7 +75,8 @@ class ParseOrchidAccountResult {
     // Try to parse as a V0 account
     try {
       return ParseOrchidAccountResult(
-          account: OrchidAccountConfigV0.parseOrchidAccount(config, existingKeys));
+          account:
+              OrchidAccountConfigV0.parseOrchidAccount(config, existingKeys));
     } catch (err) {
       if (v0Only) {
         throw err;
@@ -63,8 +84,17 @@ class ParseOrchidAccountResult {
     }
 
     // Try to parse as a V1 identity (which may be a subset of a V0 account)
+    try {
+      return ParseOrchidAccountResult(
+          identity:
+              OrchidAccountConfigV1.parseOrchidIdentity(config, existingKeys));
+    } catch (err) {
+      // fall through to raw key test
+    }
+
+    // Try to parse as a raw hex key
     return ParseOrchidAccountResult(
-        identity: OrchidAccountConfigV1.parseOrchidIdentity(config, existingKeys));
+        identity: OrchidAccountConfigV1.parseRawKey(config, existingKeys));
   }
 
   @override
