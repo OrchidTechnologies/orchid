bb2394632932f5b80c3c69af3e0027d5c8592303
Jay Freeman (saurik) <saurik@saurik.com>
2021-11-19 06:24:23 +0000

    Upgrade many dependencies to their latest version.

--- env/submodule.sh
+++ env/submodule.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 # XXX: flutter needs deep for an unknown reason; maybe this can be fixed?
+# XXX: googletest broke recently and doesn't even support HEAD and I sigh
 # XXX: the other repositories need uploadpack.allowReachableSHA1InWant :(
-git submodule update --init --recursive "$@" -- app-shared/flutter min-cairo/libpng min-glib/{gnulib,libiconv} p2p/lwip vpn-shared/{libssh,tor}
+git submodule update --init --recursive "$@" -- app-shared/flutter min-cairo/libpng min-glib/{gnulib,libiconv} min-zlib/googletest p2p/lwip vpn-shared/{libssh,tor}
 git submodule update --init --recursive "$@" --depth 1
