395ce208dfdfa657449e203d7c7b4d6b2f937e62
Jay Freeman (saurik) <saurik@saurik.com>
2022-03-09 11:40:36 +0000

    Improve reliability of CentOS vault RPM downloads.

--- env/sys-centos6.sh
+++ env/sys-centos6.sh
@@ -11,14 +11,21 @@ rm -rf "${sysroot}"
 mkdir -p "${sysroot}"
 pushd "${sysroot}"
 
-#vault=https://vault.centos.org
-vault=https://archive.kernel.org/centos-vault
-#vault=http://linuxsoft.cern.ch/centos-vault
-#vault=http://mirror.nsc.liu.se/centos-store
+vaults=()
+vaults+=(https://vault.centos.org)
+vaults+=(https://archive.kernel.org/centos-vault)
+#vaults+=(http://linuxsoft.cern.ch/centos-vault)
+#vaults+=(http://mirror.nsc.liu.se/centos-store)
 
 function rpm() {
-    curl -s "${vault}"/6.0/os/"${machine}"/Packages/"$1".el6."${arch}".rpm | rpm2cpio - | cpio -i
-    find . -type d ! -perm 755 -exec chmod 755 {} +
+    for vault in "${vaults[@]}"; do
+        if curl -s -o "$1".rpm -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36' "${vault}"/6.0/os/"${machine}"/Packages/"$1".el6."${arch}".rpm; then
+            rpm2cpio "$1".rpm | cpio -i
+            find . -type d ! -perm 755 -exec chmod 755 {} +
+            return
+        fi
+    done
+    false
 }
 
 rpm filesystem-2.4.30-2.1
