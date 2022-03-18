30f287f6e8da10c8f8a79d89900ba23736ff847d
Jay Freeman (saurik) <saurik@saurik.com>
2022-03-17 22:38:23 -0700

    Xcode 13's clang is broken, but I may as well try.

--- p2p/asio.mk
+++ p2p/asio.mk
@@ -93,3 +93,8 @@ cflags += -DBOOST_ASIO_DISABLE_CONNECTEX
 
 # XXX: this is because I am still using an old version of libc++
 cflags += -DBOOST_FILESYSTEM_NO_CXX20_ATOMIC_REF
+
+ifeq ($(target),mac)
+# the MacOS12 SDK unconditionally defines _LIBCPP_HAS_ALIGNED_ALLOC but it requires MacOS 10.15+
+cflags += -DBOOST_ASIO_DISABLE_STD_ALIGNED_ALLOC
+endif
