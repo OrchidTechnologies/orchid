d9b811a9fa1b2ad2f72e59360452e2642291ee29
Jay Freeman (saurik) <saurik@saurik.com>
2022-05-11 00:57:24 -0700

    Work around availability mistake from Xcode 12.5+.

--- min-webrtc/extra/absl/base/options.h
+++ min-webrtc/extra/absl/base/options.h
@@ -1,3 +1,9 @@
 #include_next <absl/base/options.h>
+// XXX: maybe only #ifdef __APPLE__?
+// iOS 11 does NOT have std::variant
+#undef ABSL_OPTION_USE_STD_ANY
+#define ABSL_OPTION_USE_STD_ANY 0
 #undef ABSL_OPTION_USE_STD_OPTIONAL
 #define ABSL_OPTION_USE_STD_OPTIONAL 0
+#undef ABSL_OPTION_USE_STD_VARIANT
+#define ABSL_OPTION_USE_STD_VARIANT 0
