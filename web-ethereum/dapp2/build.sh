65e8ca1a823b1f8659c545dc079b1f1d3a5c8e27
Patrick Niemeyer <pat@pat.net>
2022-08-30 11:49:12 -0500

    dapp: Fix build for linux.

--- web-ethereum/dapp2/build.sh
+++ web-ethereum/dapp2/build.sh
@@ -10,6 +10,6 @@ $FLUTTER build web --web-renderer canvaskit
 
 # move js to hash named file
 uuid=$(uuidgen)
-sed -i '' "s/main.dart.js/${uuid}.main.dart.js/g" build/web/index.html
+gsed -i "s/main.dart.js/${uuid}.main.dart.js/g" build/web/index.html
 mv build/web/main.dart.js "build/web/${uuid}.main.dart.js"
 
