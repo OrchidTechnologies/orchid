ded92b05a9584230c21edf58631f90ac466cb0f9
Jay Freeman (saurik) <saurik@saurik.com>
2022-12-17 18:35:31 -0800

    Update Android SDK components for barcode scanner.

--- env/setup-ndk.sh
+++ env/setup-ndk.sh
@@ -9,7 +9,7 @@ else
     "${ANDROID_HOME}"/tools/bin/sdkmanager --uninstall "ndk-bundle" >/dev/null
 fi
 
-echo y | "${ANDROID_HOME}"/tools/bin/sdkmanager "build-tools;29.0.2" "ndk;25.1.8937393" "platforms;android-30" >/dev/null
+echo y | "${ANDROID_HOME}"/tools/bin/sdkmanager "build-tools;29.0.2" "ndk;25.1.8937393" "platforms;android-31" >/dev/null
 
 # XXX: this is really *seriously* wrong, but GitHub is going out of their way to set ANDROID_NDK_HOME to their bogus path
 # XXX: the correct fix here is for GitHub to stop setting that at all; barring that, I need to un-set it at the top level
