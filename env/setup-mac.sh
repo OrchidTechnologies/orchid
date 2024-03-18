1101ee514e93639d354c27377751ec68aecf5749
Jay Freeman (saurik) <saurik@saurik.com>
2024-03-18 02:29:57 +0000

    i forgot the actual fix :/

--- env/setup-mac.sh
+++ env/setup-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 which brew &>/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install autoconf autoconf-archive automake capnp fakeroot groff libtool meson rpm2cpio rustup-init zstd
+brew install autoconf autoconf-archive automake binutils capnp fakeroot groff libtool meson rpm2cpio rustup-init zstd
 rustup-init -y --no-modify-path --no-update-default-toolchain
 env/setup-all.sh
