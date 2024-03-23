dde79482b8120dd9fb6ca56bb709d4c681314bd6
Jay Freeman (saurik) <saurik@saurik.com>
2024-03-23 00:04:57 -0700

    Provide at least some support for build from Arch.

---
+++ env/lnx-arch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+pacman -Sy \
+    ubuntu-keyring \
+    bc tcl vim \
+    curl git rsync wget \
+    fakeroot talloc \
+    cpio rpm-tools unzip zstd \
+    clang lld llvm \
+    binutils aarch64-linux-gnu-binutils \
+    libc++ libc++abi \
+    python-pip python-setuptools \
+    jdk17-openjdk \
+    bison flex gperf \
+    gettext groff texinfo \
+    autoconf autoconf-archive automake \
+    libtool meson ninja pkgconf \
+    qemu-system-{x86,aarch64} \
--- env/lnx-ubuntu.sh
+++ env/lnx-ubuntu.sh
@@ -5,20 +5,21 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 
 apt-get -y install \
+    ubuntu-keyring \
     bc tcl xxd \
     curl git-core rsync wget \
     fakeroot libtalloc-dev \
     cpio rpm unzip zstd \
     clang clang-tidy lld llvm \
-    binutils-{aarch64,x86-64}-linux-gnu \
+    binutils-{x86-64,aarch64}-linux-gnu \
     libc++-dev libc++abi-dev \
-    g++-multilib gcc-multilib \
     python3-pip python3-setuptools \
     openjdk-11-jre-headless \
     bison flex gperf \
     gettext groff texinfo \
     autoconf autoconf-archive automake \
     libtool ninja-build pkg-config \
+    qemu-system-{x86,arm} \
 
 function usable() {
     # Ubuntu bionic ships meson 0.45, which is too old to build glib
