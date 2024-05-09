5b11e25193ac191d766eb6cbf9084092f8857291
Jay Freeman (saurik) <saurik@saurik.com>
2024-05-09 01:07:04 +0000

    Commit a (still unused) contract addresses header.

---
+++ p2p/source/contract.hpp
@@ -0,0 +1,35 @@
+/* Orchid - WebRTC P2P VPN Market (on Ethereum)
+ * Copyright (C) 2017-2020  The Orchid Authors
+*/
+
+/* GNU Affero General Public License, Version 3 {{{ */
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or CONTRACTNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+/* }}} */
+
+
+#ifndef ORCHID_CONTRACT_HPP
+#define ORCHID_CONTRACT_HPP
+
+#include "address.hpp"
+
+namespace orc {
+
+static constexpr Address Lottery0_("0xb02396f06CC894834b7934ecF8c8E5Ab5C1d12F1");
+static constexpr Address Lottery1_("0x6dB8381b2B41b74E17F5D4eB82E8d5b04ddA0a82");
+
+}
+
+#endif//ORCHID_CONTRACT_HPP
