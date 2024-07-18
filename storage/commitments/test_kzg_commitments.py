0f1328d0c6f60e8d06f43a0e748ee1e620381e35
Patrick Niemeyer <pat@pat.net>
2024-07-17 22:34:30 -0500

    storage: Tweak example code.

--- storage/commitments/test_kzg_commitments.py
+++ storage/commitments/test_kzg_commitments.py
@@ -6,10 +6,7 @@ from commitments.kzg_commitment import get_kzg_setup, FileCommitments
 
 # Generate a random blob: 4096 field elements.
 def random_blob() -> bytes:
-    blob = b''
-    for i in range(4096):
-        blob += FileCommitments.random_field_element()
-    return blob
+    return b''.join(FileCommitments.random_field_element() for _ in range(4096))
 
 
 # Simulate corruption: Flip one bit in the blob
