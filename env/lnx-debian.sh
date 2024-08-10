1a3c0f73ad53332cd95673c2296454495059e3c3
Jay Freeman (saurik) <saurik@saurik.com>
2024-08-10 02:34:45 +0000

    Support non-Ubuntu Debian dependency installation.

---
+++ env/lnx-debian.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+
+apt-get -y install \
+    ubuntu-keyring \
+    bc tcl xxd \
+    curl git-core rsync wget \
+    fakeroot libtalloc-dev \
+    cpio rpm unzip zstd \
+    clang clang-tidy lld llvm \
+    binutils-{x86-64,aarch64}-linux-gnu \
+    binutils-mingw-w64-{i686,x86-64} \
+    libc++-dev libc++abi-dev \
+    python3-{packaging,pip,setuptools} \
+    openjdk-11-jre-headless \
+    bison flex gperf \
+    gettext groff texinfo \
+    autoconf{,-archive} automake libtool \
+    make meson ninja-build pkg-config \
+    qemu-system-{x86,arm} \
+
+if dpkg --compare-versions $(meson --version) '<<' "1.2.0"; then
+    pip3 install --upgrade meson
+fi
