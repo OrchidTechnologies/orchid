3923b18dead84587fc33f117ad4032fd3bf4520d
Jay Freeman (saurik) <saurik@saurik.com>
2024-11-19 05:48:46 +0000

    Further improve the annoying tunnel shell scripts.

--- srv-daemon/bridge.sh
+++ srv-daemon/bridge.sh
@@ -5,24 +5,30 @@ set -ex
 #https://terrywang.net/2016/02/02/new-iptables-gotchas.html
 #https://davidhamann.de/2017/04/19/sharing-vpn-on-macos/
 
-sysctl -w net.ipv4.ip_forward=1
+tun=$1
+dev=$2
+shift 2
 
-dev=$1
 ip4=$(ip address show dev $dev | sed -e '/^ *inet /!d;s///;s@/.*@@')
 gw4=${ip4%.*}.1
 
-ip tuntap add dev tun0 mode tun
-ifconfig tun0 $ip4 pointtopoint $ip4
+ip tuntap del dev $tun mode tun
+ip tuntap add dev $tun mode tun
+ifconfig $tun $ip4 pointtopoint $ip4
 
+ip route delete table local $ip4 dev $tun
 ip route delete table local $ip4 dev $dev
-ip route delete table local $ip4 dev tun0
 
 ip route add table 7 $gw4 dev $dev
 ip route add table 7 default via $gw4 dev $dev
-ip route add table 7 $ip4 dev tun0
+ip route add table 7 $ip4 dev $tun
 
+ip rule add iif $tun table 7
 ip rule add iif $dev table 7
-ip rule add iif tun0 table 7
 
-ip route get $ip4 from $gw4 iif $dev
-ip route get $gw4 from $ip4 iif tun0
+sysctl -w net.ipv4.ip_forward=1
+iptables -A FORWARD -i $tun -o $dev -j ACCEPT
+iptables -A FORWARD -i $dev -o $tun -j ACCEPT
+
+ip route get 1.0.0.1 from $ip4 iif $tun
+ip route get $ip4 from 1.0.0.1 iif $dev
--- srv-daemon/ipmasq.sh
+++ srv-daemon/ipmasq.sh
@@ -2,13 +2,22 @@
 
 set -ex
 
-sysctl -w net.ipv4.ip_forward=1
+tun=$1
+dev=$2
+shift 2
 
-dev=$1
 ip4=192.168.0.5
 gw4=${ip4%.*}.1
+net=${ip4%.*}.0/24
 
-ip tuntap add dev tun0 mode tun
-ifconfig tun0 $gw4 pointtopoint $ip4
-iptables -t nat -A POSTROUTING -s $ip4 -o $dev -j MASQUERADE
-ip route get 1.0.0.1 from $ip4 iif tun0
+ip tuntap del dev $tun mode tun
+ip tuntap add dev $tun mode tun
+ifconfig $tun $gw4 pointtopoint $ip4
+
+iptables -t nat -A POSTROUTING -s $net -o $dev -j MASQUERADE
+
+sysctl -w net.ipv4.ip_forward=1
+iptables -A FORWARD -i $tun -o $dev -j ACCEPT
+iptables -A FORWARD -i $dev -o $tun -j ACCEPT
+
+ip route get 1.0.0.1 from $ip4 iif $tun
