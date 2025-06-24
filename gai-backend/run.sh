6725a787003ede35e77200ab51f8b11c953f7972
Nick <2539919+Count042@users.noreply.github.com>
2025-06-24 12:42:16 -0700

    Updated Dockerfile for gai-backend, fixed entrypoint script to handle both server functions

--- gai-backend/run.sh
+++ gai-backend/run.sh
@@ -7,14 +7,17 @@ file_env() {
     local fileVar="${var}_FILE"
     local def="${2:-}"
 
-    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; 
+    then
         echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
         exit 1
     fi
     local val="$def"
-    if [ "${!var:-}" ]; then
+    if [ "${!var:-}" ]; 
+    then
         val="${!var}"
-    elif [ "${!fileVar:-}" ]; then
+    elif [ "${!fileVar:-}" ]; 
+    then
         val="$(< "${!fileVar}")"
     fi
     export "$var"="$val"
@@ -25,10 +28,29 @@ file_env "ORCHID_GENAI_ADDR"
 file_env "ORCHID_GENAI_PORT"
 file_env "ORCHID_GENAI_REDIS_URL"
 file_env "ORCHID_GENAI_RECIPIENT_KEY"
+file_env "ORCHID_GENAI_BILLING"
+file_env "ORCHID_GENAI_INFERENCE"
+
+if [[ "$ORCHID_GENAI_BILLING" == "$ORCHID_GENAI_INFERENCE" ]] ;
+then
+    echo >&2 'error: both $ORCHID_GENAI_BILLING and $ORCHID_GENAI_INFERENCE are set (but are exclusive), or neither is set and one is required'
+    exit 1
+fi
+
+if [[ "$ORCHID_GENAI_INFERENCE" == "true" ]] ;
+then
+    command="uvicorn inference_api:app --host 0.0.0.0 --port 8010;"
+elif [[ "$ORCHID_GENAI_BILLING" == "true" ]]
+then
+    command="python server.py --config config.json"
+else
+    echo >&2 'Neither $ORCHID_GENAI_BILLING or $ORCHID_GENAI_INFERENCE are set to "true", and one is required to be set to "true"'
+    exit 1
+fi
 
 while true
 do
-  python server.py;
+  eval $command;
   sleep 1;
 done
 
